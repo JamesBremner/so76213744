46c3af7ec777a5bd52789a7fa66d028bef86a32d
JamesBremner <james@ravenspoint.com>
2023-05-10 17:15:51 -0400

    Remove restriction on node count

--- src/main.cpp
+++ src/main.cpp
@@ -3,194 +3,223 @@
 #include <climits>
 #include <cstring>
 #include <iostream>
-#include "GraphTheory.h"    //  https://github.com/JamesBremner/PathFinder
+#include "GraphTheory.h" //  https://github.com/JamesBremner/PathFinder
+
+// const int N = 5;
+
+int edgeWeight(const raven::graph::cGraph &g, int i, int j)
+{
+    return atoi(g.rEdgeAttr(g.find(i, j), 0).c_str());
+}
+
+// std::vector<std::vector<int>>
+// makeAdjacencyMatrix(const raven::graph::cGraph &g)
+// {
+//     std::vector<std::vector<int>> M;
+//     int N = g.vertexCount();
+//     for (int n1 = 0; n1 < N; n1++)
+//     {
+//         std::vector<int> row;
+//         for (int n2 = 0; n2 < N; n2++)
+//         {
+//             if (n1 == n2)
+//                 row.push_back(0);
+//             else
+//                 row.push_back(edgeWeight(g, n1, n2));
+//         }
+//         M.push_back(row);
+//     }
+//     return M;
+// }
+
+// raven::graph::cGraph
+// makeGraph(int adj[N][N])
+// {
+//     raven::graph::cGraph g;
+//     for (int n1 = 0; n1 < N; n1++)
+//     {
+//         for (int n2 = n1 + 1; n2 < N; n2++)
+//         {
+//             g.wEdgeAttr(
+//                 g.add(n1, n2),
+//                 {std::to_string(adj[n1][n2])});
+//         }
+//     }
+//     return g;
+// }
 
 using namespace std;
-const int N = 4;
+
 // final_path[] stores the final solution ie, the
 // path of the salesman.
-int final_path[N+1];
+std::vector<int> final_path;
+
+std::vector<int> curr_path;
 
 // visited[] keeps track of the already visited nodes
 // in a particular path
-bool visited[N];
+std::vector<bool> visited;
 
 // Stores the final minimum weight of shortest tour.
 int final_res = INT_MAX;
 
-// Function to copy temporary solution to
-// the final solution
-void copyToFinal(int curr_path[])
-{
-	for (int i=0; i<N; i++)
-		final_path[i] = curr_path[i];
-	final_path[N] = curr_path[0];
-}
-
 // Function to find the minimum edge cost
 // having an end at the vertex i
-int firstMin(int adj[N][N], int i)
+int firstMin(const raven::graph::cGraph &g, int i)
 {
-	int min = INT_MAX;
-	for (int k=0; k<N; k++)
-		if (adj[i][k]<min && i != k)
-			min = adj[i][k];
-	return min;
+    int min = INT_MAX;
+    for (int k : g.adjacentOut(i))
+    {
+        if (k != i)
+        {
+            int c = edgeWeight(g, i, k);
+            if (c < min)
+                min = c;
+        }
+    }
+    return min;
 }
 
 // function to find the second minimum edge cost
 // having an end at the vertex i
-int secondMin(int adj[N][N], int i)
+int secondMin(const raven::graph::cGraph &g, int i)
 {
-	int first = INT_MAX, second = INT_MAX;
-	for (int j=0; j<N; j++)
-	{
-		if (i == j)
-			continue;
+    int first = INT_MAX, second = INT_MAX;
+    for (int j = 0; j < g.vertexCount(); j++)
+    {
+        if (i == j)
+            continue;
 
-		if (adj[i][j] <= first)
-		{
-			second = first;
-			first = adj[i][j];
-		}
-		else if (adj[i][j] <= second &&
-				adj[i][j] != first)
-			second = adj[i][j];
-	}
-	return second;
+        int c = edgeWeight(g, i, j);
+        if (c <= first)
+        {
+            second = first;
+            first = c;
+        }
+        else if (c <= second &&
+                 c != first)
+            second = c;
+    }
+    return second;
 }
 
-// function that takes as arguments:
+// recursive function that takes as arguments:
 // curr_bound -> lower bound of the root node
 // curr_weight-> stores the weight of the path so far
 // level-> current level while moving in the search
 //		 space tree
 // curr_path[] -> where the solution is being stored which
 //			 would later be copied to final_path[]
-void TSPRec(int adj[N][N], int curr_bound, int curr_weight,
-			int level, int curr_path[])
+void TSPRec(const raven::graph::cGraph &g, int curr_bound, int curr_weight,
+            int level)
 {
-	// base case is when we have reached level N which
-	// means we have covered all the nodes once
-	if (level==N)
-	{
-		// check if there is an edge from last vertex in
-		// path back to the first vertex
-		if (adj[curr_path[level-1]][curr_path[0]] != 0)
-		{
-			// curr_res has the total weight of the
-			// solution we got
-			int curr_res = curr_weight +
-					adj[curr_path[level-1]][curr_path[0]];
+    // base case is when we have reached level N which
+    // means we have covered all the nodes once
+    if (level == g.vertexCount())
+    {
+        // check if there is an edge from last vertex in
+        // path back to the first vertex
+        if (g.find(curr_path[level - 1], curr_path[0]) >= 0)
+        {
+            // curr_res has the total weight of the
+            // solution we got
+            int curr_res = curr_weight +
+                           edgeWeight(g, curr_path[level - 1], curr_path[0]);
 
-			// Update final result and final path if
-			// current result is better.
-			if (curr_res < final_res)
-			{
-				copyToFinal(curr_path);
-				final_res = curr_res;
-			}
-		}
-		return;
-	}
+            // Update final result and final path if
+            // current result is better.
+            if (curr_res < final_res)
+            {
+                final_path = curr_path;
+                final_path.push_back(curr_path[0]);
+                final_res = curr_res;
+            }
+        }
+        return;
+    }
 
-	// for any other level iterate for all vertices to
-	// build the search space tree recursively
-	for (int i=0; i<N; i++)
-	{
-		// Consider next vertex if it is not same (diagonal
-		// entry in adjacency matrix and not visited
-		// already)
-		if (adj[curr_path[level-1]][i] != 0 &&
-			visited[i] == false)
-		{
-			int temp = curr_bound;
-			curr_weight += adj[curr_path[level-1]][i];
+    // for any other level iterate for all vertices to
+    // build the search space tree recursively
+    for (int i = 0; i < g.vertexCount(); i++)
+    {
+        // Consider next vertex if it is not same (diagonal
+        // entry in adjacency matrix and not visited
+        // already)
+        if (g.find(curr_path[level - 1], i) >= 0 &&
+            visited[i] == false)
+        {
+            int temp = curr_bound;
+            curr_weight += edgeWeight(g, curr_path[level - 1], i);
 
-			// different computation of curr_bound for
-			// level 2 from the other levels
-			if (level==1)
-			curr_bound -= ((firstMin(adj, curr_path[level-1]) +
-							firstMin(adj, i))/2);
-			else
-			curr_bound -= ((secondMin(adj, curr_path[level-1]) +
-							firstMin(adj, i))/2);
+            // different computation of curr_bound for
+            // level 2 from the other levels
+            if (level == 1)
+                curr_bound -= ((firstMin(g, curr_path[level - 1]) +
+                                firstMin(g, i)) /
+                               2);
+            else
+                curr_bound -= ((secondMin(g, curr_path[level - 1]) +
+                                firstMin(g, i)) /
+                               2);
 
-			// curr_bound + curr_weight is the actual lower bound
-			// for the node that we have arrived on
-			// If current lower bound < final_res, we need to explore
-			// the node further
-			if (curr_bound + curr_weight < final_res)
-			{
-				curr_path[level] = i;
-				visited[i] = true;
+            // curr_bound + curr_weight is the actual lower bound
+            // for the node that we have arrived on
+            // If current lower bound < final_res, we need to explore
+            // the node further
+            if (curr_bound + curr_weight < final_res)
+            {
+                curr_path[level] = i;
+                visited[i] = true;
 
-				// call TSPRec for the next level
-				TSPRec(adj, curr_bound, curr_weight, level+1,
-					curr_path);
-			}
+                // call TSPRec for the next level
+                TSPRec(g, curr_bound, curr_weight, level + 1);
+            }
 
-			// Else we have to prune the node by resetting
-			// all changes to curr_weight and curr_bound
-			curr_weight -= adj[curr_path[level-1]][i];
-			curr_bound = temp;
+            // Else we have to prune the node by resetting
+            // all changes to curr_weight and curr_bound
+            curr_weight -= edgeWeight(g, curr_path[level - 1], i);
+            curr_bound = temp;
 
-			// Also reset the visited array
-			memset(visited, false, sizeof(visited));
-			for (int j=0; j<=level-1; j++)
-				visited[curr_path[j]] = true;
-		}
-	}
+            // Also reset the visited array
+            visited.clear();
+            visited.resize(g.vertexCount(), false);
+
+            for (int j = 0; j <= level - 1; j++)
+                visited[curr_path[j]] = true;
+        }
+    }
 }
 
 // This function sets up final_path[]
-void TSPBB(int adj[N][N])
+void TSPBB(const raven::graph::cGraph &g)
 {
-	int curr_path[N+1];
 
-	// Calculate initial lower bound for the root node
-	// using the formula 1/2 * (sum of first min +
-	// second min) for all edges.
-	// Also initialize the curr_path and visited array
-	int curr_bound = 0;
-	memset(curr_path, -1, sizeof(curr_path));
-	memset(visited, 0, sizeof(curr_path));
+    // Calculate initial lower bound for the root node
+    // using the formula 1/2 * (sum of first min +
+    // second min) for all edges.
+    // Also initialize the curr_path and visited array
+    int curr_bound = 0;
 
-	// Compute initial bound
-	for (int i=0; i<N; i++)
-		curr_bound += (firstMin(adj, i) +
-					secondMin(adj, i));
+    curr_path.clear();
+    curr_path.resize(g.vertexCount() + 1, -1);
 
-	// Rounding off the lower bound to an integer
-	curr_bound = (curr_bound&1)? curr_bound/2 + 1 :
-								curr_bound/2;
+    // Compute initial bound
+    // auto g = makeGraph( adj );
+    for (int i = 0; i < g.vertexCount(); i++)
+        curr_bound += (firstMin(g, i) +
+                       secondMin(g, i));
 
-	// We start at vertex 1 so the first vertex
-	// in curr_path[] is 0
-	visited[0] = true;
-	curr_path[0] = 0;
+    // Rounding off the lower bound to an integer
+    curr_bound = (curr_bound & 1) ? curr_bound / 2 + 1 : curr_bound / 2;
 
-	// Call to TSPRec for curr_weight equal to
-	// 0 and level 1
-	TSPRec(adj, curr_bound, 0, 1, curr_path);
-}
+    // We start at vertex 1 so the first vertex
+    // in curr_path[] is 0
+    visited[0] = true;
+    curr_path[0] = 0;
 
-std::vector<std::vector<int>>
-makeAdjacencyMatrix(const raven::graph::cGraph & g )
-{
-    std::vector<std::vector<int>> M;
-    int N = g.vertexCount();
-    for( int n1 = 0; n1 < N; n1++ ) {
-        std::vector<int> row;
-        for( int n2 = 0; n2 < N; n2++ ) {
-            if( n1 == n2 )
-                row.push_back(0);
-            else
-                row.push_back(atoi(g.rEdgeAttr(g.find(n1,n2),0).c_str()));
-        }
-        M.push_back( row);
-    }
-    return M;
+    // Call to TSPRec for curr_weight equal to
+    // 0 and level 1
+    TSPRec(g, curr_bound, 0, 1);
 }
 
 // Driver code
@@ -198,27 +227,18 @@ int main()
 {
 
     raven::graph::cGraph g;
-    //readfile( g, "../dat/data.txt" );
-    readfile( g, "../dat/notmetric.txt" );
-    auto M = makeAdjacencyMatrix( g );
+    // readfile( g, "../dat/data.txt" );
+    // readfile( g, "../dat/notmetric.txt" );
+    readfile(g, "../dat/so76213744.txt");
 
-    if( g.vertexCount() != N )
-        throw std::runtime_error(
-            "Unsupported vertex count"        );
-    int adj[N][N];
-    for( int n1 = 0; n1 < N; n1++ ) {
-        for( int n2 = 0; n2 < N; n2++ ) {
-            adj[n1][n2] = M[n1][n2];
-        }
-    }
+    visited.resize(g.vertexCount(), false);
 
-	TSPBB(adj);
+    TSPBB(g);
 
     cout << "Minimum cost : " << final_res << "\nPath Taken : ";
-    for( int n : final_path)
-        cout << n <<" ";
+    for (int n : final_path)
+        cout << g.userName(n) << " ";
     cout << "\n";
 
-
-	return 0;
+    return 0;
 }
